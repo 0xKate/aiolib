71a75d456b2459b6d04e703df304cc7f2a6f48ff
0xKate <94711905+0xKate@users.noreply.github.com>
2022-01-29 07:40:16 -0800

    Use new aioStreamClient API

--- ConsoleClientExample/Program.cs
+++ ConsoleClientExample/Program.cs
@@ -7,15 +7,40 @@ namespace ConsoleProgram
 {
     class ConsoleApp
     {
+        static void OnConnectCallback(object? sender, ConnectionEventArgs eventArgs)
+        {
+            if (eventArgs.Conn != null)
+                Console.WriteLine($"Connected to server {eventArgs.Conn.RemoteEndPoint}");
+        }
+        static void OnDisconnectCallback(object? sender, ConnectionEventArgs eventArgs)
+        {
+            if (eventArgs.Conn != null)
+                Console.WriteLine($"Disconnected from server {eventArgs.Conn.RemoteEndPoint}");
+        }
+        static void OnReceiveCallback(object? sender, ConnectionEventArgs eventArgs)
+        {
+            if (eventArgs.Conn != null)
+                Console.WriteLine($"Server {eventArgs.Conn.RemoteEndPoint} has sent data: {eventArgs.Message}");
+        }
+        static void OnExceptionCallback(object? sender, ConnectionEventArgs eventArgs)
+        {
+            if (eventArgs.Conn != null)
+                Console.WriteLine($"Server {eventArgs.Conn.RemoteEndPoint} has caused exception: {eventArgs.Message}");
+        }
+
         static void Main(string[] args)
         {
             try
             {
-                int port = 5000;
-                IPAddress ipAddress = IPAddress.Parse("10.0.0.10");
+                int port = 1025;
+                string hostname = "gamesys.kfuji.net";
                 bool running = true;
 
-                aioStreamClient client = new aioStreamClient(port, ipAddress);
+                aioStreamClient client = new aioStreamClient(port, hostname);
+                client.ConnReadyEvent.OnEvent += OnConnectCallback;
+                client.ConnClosedEvent.OnEvent += OnDisconnectCallback;
+                client.RecvEvent.OnEvent += OnReceiveCallback;
+                client.ConnErrorEvent.OnEvent += OnExceptionCallback;
                 client.Run();
 
                 while (running)
@@ -27,6 +52,10 @@ namespace ConsoleProgram
                     {
                         running = false;
                         break;
+                    }
+                    if (userInput == "restart")
+                    {
+                        client.Run();
                     }
                     else
                     {
