83805c6ba3d1c8e1f8d178ccedf7ffa09466a7cc
0xKate <94711905+0xKate@users.noreply.github.com>
2022-01-29 07:42:33 -0800

    Misc

---
+++ TestServerUI/Users.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestServerUI
+{
+
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    class Users
+    {
+        public class User : ObservableObject
+        {
+            public String UserName { get; set; }
+            public IPEndPoint UserEndPoint { get; set; }
+
+            public User(String UserName, IPEndPoint UserEndPoint)
+            {
+                this.UserName = UserName;
+                this.UserEndPoint = UserEndPoint;
+            }
+        }
+    }
+}
