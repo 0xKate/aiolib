e051d94694d9c4f3651b94a413693c383b043712
0xKate <94711905+0xKate@users.noreply.github.com>
2022-01-29 07:42:15 -0800

    Add MakeCert Button

--- TestServerUI/MainWindow.xaml.cs
+++ TestServerUI/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TestServerUI
 {
@@ -24,12 +12,17 @@ namespace TestServerUI
         public MainWindow()
         {
             InitializeComponent();
-            Console.WriteLine("MainWindow Initialized.");
+            //Console.WriteLine("MainWindow Initialized.");
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            Console.WriteLine(e.AddedItems.ToString());
+        }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            aiolib.Crypto.MakeCert("test.crt", "test.key", "gamesys.kfuji.net", "10.0.0.10");
         }
     }
 }
